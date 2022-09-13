0c0f467d1ce5dbbf79d036fb8ff3aea0bc1d9885
Samuel Matos <samuhmatos@gmail.com>
2022-09-13 13:12:04 -0300

    footer done

--- Disney+ 2.0/js/functions.js
+++ Disney+ 2.0/js/functions.js
@@ -11,4 +11,35 @@ $(function() {
             $(this).parent().find('.faq-toggle').slideToggle();
         })
     }
+
+    footer()
+
+    function footer() {
+        lang()
+
+        function lang(){
+            var btn_lang = $('.lang-toggle')
+            lang_toggle();
+            hover_lang();
+
+            function hover_lang(){
+                btn_lang.hover(function(){
+                    $(this).find('p').css('textDecoration','underline');
+                    $(this).find('i').css('color', 'white');
+                    $('#svg-lang').css('stroke', 'white');
+                }, function(){
+                    $(this).find('p').css('textDecoration','none');
+                    $(this).find('i').css('color', 'silver');
+                    $(this).find('svg').css('stroke', 'silver');
+                })
+            }
+            function lang_toggle() {
+                btn_lang.on('click', function(){
+                    var sub_lang = $('.sub-lang')
+                    sub_lang.slideToggle()
+                })
+            }
+        }
+        
+    }
 });
