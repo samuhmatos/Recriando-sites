7b26535fadfcd5e0db1fd2e04308a74fff969b61
Samuel Matos <samuhmatos@gmail.com>
2022-09-13 10:10:34 -0300

    faq done

--- Disney+ 2.0/js/functions.js
+++ Disney+ 2.0/js/functions.js
@@ -1,2 +1,14 @@
 $(function() {
+    faq_toggle();
+
+    function faq_toggle() {
+
+        var btn_faq = $('.btn-faq');
+
+        btn_faq.on('click', function(){
+            $(this).find('svg polyline:first-of-type').toggle();
+            //$('.faq-toggle').slideUp()
+            $(this).parent().find('.faq-toggle').slideToggle();
+        })
+    }
 });
